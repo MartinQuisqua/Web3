e2ac0348a3f6b1cad640cbb370bc74d6d9527ba4
Quisquater Martin <martin.quisquater@student.vinci.be>
2023-09-15 10:24:51 +0200

    1.1-1.2 : import image

--- exercises/module1/part1/src/components/Header/Header.jsx
+++ exercises/module1/part1/src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import logo from './LOGO.png';
 const Header = (props) => {
     return (
       <div>
         <h1>{props.course}</h1> 
+        <img src={logo} alt="logo" />
       </div>
     )
   }
