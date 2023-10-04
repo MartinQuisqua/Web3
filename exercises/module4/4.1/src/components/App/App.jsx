c14d5ef85e7b2614fd8de0582ea0ea77e1193dd4
Quisquater Martin <martin.quisquater@student.vinci.be>
2023-10-04 15:24:25 +0200

    ajout api ex

--- exercises/module4/4.1/src/components/App/App.jsx
+++ exercises/module4/4.1/src/components/App/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", num: "040-123456", id: 1 },
-    { name: "Ada Lovelace", num: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", num: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", num: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
-
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
